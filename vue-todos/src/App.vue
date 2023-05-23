bf91a471b6b53d7e31b5354772e749a5f61b5042
johnsantosDev <johnndiwayesusantos@gmail.com>
2023-05-23 19:17:58 +0200

    [new] initial commit for vue-todostutorial

---
+++ vue-todos/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+</script>
+
+<template>
+  <TodoHeader />
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet');
+* {
+  font-family: "Poppins", sans-serif;
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
+
